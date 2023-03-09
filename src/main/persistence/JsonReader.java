b56dac793bded84e1a9e267f85a4bc248aaaa773
William Gao <gao.william0206@gmail.com>
2023-03-08 20:02:33 -0800

    Implemented JsonReader Class

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,6 +1,7 @@
 package persistence;
 
 
+import model.CreditCard;
 import model.Expense;
 import model.ExpenseList;
 import model.Wallet;
@@ -28,12 +29,15 @@ public class JsonReader {
     public ExpenseList readExpenseList() throws IOException {
         String jsonData = readFile(source);
         JSONObject jsonObject = new JSONObject(jsonData);
+        return parseExpenseList(jsonObject);
     }
 
     //EFFECTS: reads and returns wallet form file
     //throws IO exception for reading errors
     public Wallet readWallet() throws IOException {
         String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseWallet(jsonObject);
     }
 
     //EFFECTS: reads and return file as string.
@@ -46,7 +50,7 @@ public class JsonReader {
         return stringBuilder.toString();
     }
 
-
+    //EFFECTS: parses expense list from JSON object and returns it.
     private ExpenseList parseExpenseList(JSONObject jsonObject) {
         ExpenseList expenseList = new ExpenseList();
         JSONObject expenseListJson = jsonObject.getJSONObject("expenseList");
@@ -54,11 +58,62 @@ public class JsonReader {
         int currentExpense = expenseListJson.getInt("currentExpense");
         JSONArray expenseArray = expenseListJson.getJSONArray("expenseList");
 
+        for (Object json : expenseArray) {
+            JSONObject nextExpense = (JSONObject) json;
+            addExpense(expenseList, nextExpense);
+        }
+
+        if (currentExpense != expenseList.getCurrentExpenses()) {
+            System.out.println("Error while parsing expense list data. ");
+            expenseList.setCurrentExpenses(currentExpense);
+        }
+
         return expenseList;
     }
 
+    //EFFECTS: parses wallet from JSON object and returns it.
     private Wallet parseWallet(JSONObject jsonObject) {
+        Wallet wallet = new Wallet();
+        JSONObject walletJson = jsonObject.getJSONObject("wallet");
+
+        int cash = walletJson.getInt("cash");
+        int bankBalance = walletJson.getInt("bankBalance");
+        JSONArray creditCardArray = walletJson.getJSONArray("cards");
+
+        for (Object json : creditCardArray) {
+            JSONObject nextCard = (JSONObject) json;
+            addCreditCard(wallet, nextCard);
+        }
+
+        wallet.setBankBalance(bankBalance);
+        wallet.setCash(cash);
+
+        return wallet;
     }
 
+    //MODIFIES: expenseList
+    //EFFECTS: parses expense from JSON object and adds it to expenseList
+    private void addExpense(ExpenseList expenseList, JSONObject nextExpense) {
+        Expense newExpense = new Expense(
+                nextExpense.getInt("cost"),
+                nextExpense.getString("name"),
+                nextExpense.getString("comments"),
+                nextExpense.getString("purchaseType"),
+                nextExpense.getString("purchaseDate"),
+                nextExpense.getBoolean("recurring"));
+
+        expenseList.addExpense(newExpense);
+    }
+
+    //MODIFIES: wallet
+    //EFFECTS: parses credit Card from JSON object and adds it to wallet
+    private void addCreditCard(Wallet wallet, JSONObject nextCard) {
+        CreditCard newCreditCard = new CreditCard(
+                nextCard.getString("name"),
+                nextCard.getInt("balance"),
+                nextCard.getInt("limit"));
+
+        wallet.addCreditCard(newCreditCard);
+    }
 
 }
