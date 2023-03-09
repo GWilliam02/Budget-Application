3764d577d60127084605dcde71c63dc918a629e1
William Gao <gao.william0206@gmail.com>
2023-03-08 20:34:26 -0800

    Implemented JsonWriter.

--- src/main/model/Budget.java
+++ src/main/model/Budget.java
@@ -1,6 +1,9 @@
 package model;
 
-public class Budget {
+import org.json.JSONObject;
+
+//Budget represents the state of the whole app and includes each component.
+public class Budget{
 
     private Wallet wallet;
     private ExpenseList expenseList;
@@ -9,6 +12,16 @@ public class Budget {
         wallet = new Wallet();
         expenseList = new ExpenseList();
     }
+
+    //EFFECTS; returns this as JSON object
+    public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("wallet", wallet);
+        json.put("expenseList", expenseList);
+        return json;
+    }
+
+
 
     public ExpenseList getExpenseList() {
         return expenseList;
--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,13 +1,16 @@
 package persistence;
 
 
-import java.io.File;
+import model.Budget;
+import org.json.JSONObject;
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 //Writer that writes budget information to a file in JSON.
 public class JsonWriter {
 
+    private static final int TAB = 4;
     private String destination;
     private PrintWriter writer;
 
@@ -25,5 +28,16 @@ public class JsonWriter {
 
     //MODIFIES: this
     //EFFECTS: writes JSON version of budgeting app to file
-    public void write(){}
+    public void write(Budget budget){
+        JSONObject json = budget.toJson();
+        saveToFile(json.toString(TAB));
+    }
+
+    //MODIFIES: this
+    //EFFECTS: closes writer
+    public void close() {writer.close();}
+
+    //MODIFIES: this
+    //EFFECTS: writes passed in string to file
+    public void saveToFile(String json) {writer.print(json);}
 }
